468955e2b0c19e7053bc716aef65812fcff19de5
vigneshvasudevan <vigneshkutta@gmail.com>
2022-12-16 21:41:35 +0530

    Create a01HelloWorld.java

---
+++ src/basics/a01HelloWorld.java
@@ -0,0 +1,11 @@
+package src.basics;
+// class name should match the filename
+public class a01HelloWorld {
+
+    // main the entry point to the application
+    public static void main(String[] args) {
+        // To print a message on the console
+        System.out.print("Hello World program");
+        System.out.println("Hi");
+    }
+}
