008f5afe525e7a33c98e033dcfa5a90f818aeaa1
vigneshvasudevan <vigneshkutta@gmail.com>
2023-01-22 21:35:00 +0530

    Add files via upload

--- src/basics/a08Functions.java
+++ src/basics/a08Functions.java
@@ -50,7 +50,42 @@ public class a08Functions {
 
     // Functions with variable length args aka variadic functions
     static void print(String... messages) {
-        
+
+    }
+    
+    public static boolean isPalindrome(String input) {
+        String s = input.toUpperCase();
+        int length = s.length();
+        int mid = Math.floorDiv(length, 2);
+        for (int i = 0 ; i <= mid; i++) {
+            if (s.charAt(i) != s.charAt(length - 1- i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+   public static void printVowConstantCount(String s) {
+        int vowCount = 0;
+        for (int i = 0; i < s.length(); i ++) {
+            switch (s.charAt(i)) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                vowCount++;
+                break;
+            }
+        }
+        System.out.format("vowel count = %d\n", vowCount);
+        System.out.format("constant count = %d\n", s.length() - vowCount);
+    }
+
+   public static void main(String[] args) {
+        System.out.println(isPalindrome("malayalam"));
+        System.out.println(isPalindrome("Hello"));
+        printVowConstantCount("");
     }
 }
     
